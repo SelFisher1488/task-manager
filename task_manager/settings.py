2e9f03782943fb39e023993ea0f23537457be53f
Denis Komandyr <sabsdaid@gmail.com>
2023-11-27 00:09:25 +0200

    Add task-manager-o8qw.onrender.com

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-7&1jn!8+b6-5vy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "task-manager-o8qw.onrender.com"]
 
 
 # Application definition
